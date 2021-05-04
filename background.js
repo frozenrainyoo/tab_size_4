6388a5b1d20d18bfff7018106af92ce63bb76c44
frozenrainyoo <frozenrain.yoo@gmail.com>
2021-05-04 14:18:36 +0900

    Added 'recover tab size' function
    Shortcut - Ctrl + Shift + E

--- background.js
+++ background.js
@@ -2,6 +2,10 @@ function customTabPage() {
   document.body.style = 'tab-size: 4';
 }
 
+function recoverTabPage() {
+  document.body.style = 'tab-size: 8';
+}
+
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
@@ -15,6 +19,11 @@ chrome.commands.onCommand.addListener(function(command) {
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
         function: customTabPage
+      });
+    } else if (command == 'toggle-recover-tab-size') {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: recoverTabPage
       });
     }
   });
